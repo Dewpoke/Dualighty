3d29ffbfcd64738abc577f08d52f34a16c5bcab5
Lee10 <leembuyisa22@gmail.com>
2019-08-28 11:28:07 +0200

    Menu Scene with the instructions

--- DualityUnityProject/Assets/Scenes/Menu.cs
+++ DualityUnityProject/Assets/Scenes/Menu.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject options;
    public void Play()
     {
         SceneManager.LoadScene("LevelOne");
@@ -12,5 +15,17 @@ public class Menu : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+
+    }
+    public void MainMenu()
+    {
+        mainMenu.SetActive(true);
+        options.SetActive(false);
     }
 }
