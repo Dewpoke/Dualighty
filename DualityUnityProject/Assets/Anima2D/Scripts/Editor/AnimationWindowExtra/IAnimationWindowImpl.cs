85a8942bc3890049e81a01dad7067b5eb6181a07
TalithaTJ1 <1665410@students.wits.ac.za>
2019-09-17 12:56:34 +0200

    Clouds added to BGAssets

--- DualityUnityProject/Assets/Anima2D/Scripts/Editor/AnimationWindowExtra/IAnimationWindowImpl.cs
+++ DualityUnityProject/Assets/Anima2D/Scripts/Editor/AnimationWindowExtra/IAnimationWindowImpl.cs
@@ -4,7 +4,6 @@ using System.Collections;
 
 public interface IAnimationWindowImpl
 {
-	EditorWindow animationWindow { get; }
 	int frame { get; set; }
 	bool recording { get; set; }
 	AnimationClip activeAnimationClip { get; }
@@ -17,4 +16,6 @@ public interface IAnimationWindowImpl
 	void InitializeReflection();
 	float FrameToTime(int frame);
 	float TimeToFrame(float time);
+	void CreateDefaultCurve(EditorCurveBinding binding);
+	void AddKey(EditorCurveBinding binding, float time);
 }
