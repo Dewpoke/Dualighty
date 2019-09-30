63453b819ea447d774f76cbfd73a153acb5bebf1
alexi <alexi16.f@gmail.com>
2019-09-30 12:36:19 +0200

    Angel and Devil animation script added.

---
+++ DualityUnityProject/Assets/Scripts/PlatformChangeVisualScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformChangeVisualScript : MonoBehaviour
+{
+    public Animation anim;
+
+    public int operatingState; //0 is once-off, 1 is on and off.
+    bool isActive;
+    bool isReadyToSwap = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive != this.GetComponent<PlatformChangeScript>().getIsActive())
+        {
+            isActive = !isActive;
+            if (isActive)
+            {
+                StartAnimation();
+            }
+            else
+            {
+                StopAnimation();
+            }
+        }
+    }
+
+    void StopAnimation()
+    {
+        anim.Stop();
+    }
+
+    void StartAnimation()
+    {
+        anim.Play();
+    }
+}
