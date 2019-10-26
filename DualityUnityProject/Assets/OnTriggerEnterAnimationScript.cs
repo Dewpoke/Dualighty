3221e67f3524a71ae9710d0eeb599ae46af8432c
alexi <alexi16.f@gmail.com>
2019-10-26 22:33:50 +0200

    Fixed angel being launched up when stuck in platform. Adjusted level 2 moving platforms quite heavily. Fixed animation for end door. Still need to add a block for end door. Still need to add subtle arrows.

---
+++ DualityUnityProject/Assets/OnTriggerEnterAnimationScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerEnterAnimationScript : MonoBehaviour
+{
+    Animation animToPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animToPlay = this.GetComponent<Animation>();
+        animToPlay.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animToPlay.Play();
+    }
+}
