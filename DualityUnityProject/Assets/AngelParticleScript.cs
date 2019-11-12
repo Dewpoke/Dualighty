956eac21af40428cce33c7f360391b63d4a481e3
alexi <alexi16.f@gmail.com>
2019-11-12 16:13:11 +0200

    Particle system semi-working

---
+++ DualityUnityProject/Assets/AngelParticleScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngelParticleScript : MonoBehaviour
+{
+    public GameObject runParticles;
+    public GameObject jumpParticles;
+
+    ParticleSystem runPartSys;
+    ParticleSystem jumpPartSys;
+
+    LightOrbController lightScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightScript = this.GetComponent<LightOrbController>();
+
+        runPartSys = runParticles.GetComponent<ParticleSystem>();
+        jumpPartSys = jumpParticles.GetComponent<ParticleSystem>();
+
+        runPartSys.Stop();
+        jumpPartSys.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightScript.GetIsGrounded() && Mathf.Abs(lightScript.GetXSpeed()) > 0.2f)//is walking
+        {
+            runPartSys.Play();
+            jumpPartSys.Stop();
+        }
+        else if (lightScript.GetIsGrounded() && Input.GetKeyDown(KeyCode.Space) && lightScript.getIsControlsActive())//jumping
+        {
+            runPartSys.Stop();
+            jumpPartSys.Play();
+        }
+        else
+        {
+            runPartSys.Stop();
+            jumpPartSys.Stop();
+        }
+    }
+}
---
+++ DualityUnityProject/Assets/DevilParticleScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevilParticleScript : MonoBehaviour
+{
+    public GameObject runParticles;
+    public GameObject jumpParticles;
+
+    ParticleSystem runPartSys;
+    ParticleSystem jumpPartSys;
+
+    DarkOrbController darkScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        darkScript = this.GetComponent<DarkOrbController>();
+
+        runPartSys = runParticles.GetComponent<ParticleSystem>();
+        jumpPartSys = jumpParticles.GetComponent<ParticleSystem>();
+
+        runPartSys.Stop();
+        jumpPartSys.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (darkScript.GetIsGrounded() && Mathf.Abs(darkScript.GetXSpeed()) > 0.2f)//is walking
+        {
+            runPartSys.Play();
+            jumpPartSys.Stop();
+        }
+        else if (darkScript.GetIsGrounded() && Input.GetKeyDown(KeyCode.Space) && darkScript.getIsControlsActive())//jumping
+        {
+            runPartSys.Stop();
+            jumpPartSys.Play();
+
+        }
+        else
+        {
+            runPartSys.Stop();
+            jumpPartSys.Stop();
+        }
+    }
+}
