70c76a15e83480c6ee08c05d3da6f41bd8350edb
TalithaTJ1 <1665410@students.wits.ac.za>
2019-11-17 17:18:26 +0200

    Added cutscenes 2 and 3

---
+++ DualityUnityProject/Assets/Scenes/CutScenes/CutSceneExit2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutSceneExit2 : MonoBehaviour
+{
+
+    public float Countdown = 22f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Countdown -= Time.deltaTime;
+        if (Countdown <= 0f)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+    }
+}
---
+++ DualityUnityProject/Assets/Scenes/CutScenes/SkipScript1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SkipScript1 : MonoBehaviour
+{
+    public GameObject Skip;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("LevelTwo");
+    }
+}
+
